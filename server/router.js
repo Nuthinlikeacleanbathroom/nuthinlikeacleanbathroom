72f2c6a226dbcc008fbe0cb739e0f207f8991c72
John Soo <jsoo1@asu.edu>
2016-02-18 15:59:00 -0800

    Added client directory, routers

--- server/router.js
+++ server/router.js
@@ -1,6 +1,6 @@
 module.exports = function(app) {
-  app.route('/')
-  .get(function(req, res, next) {
-    res.json('Hello World');
-  });
+  // app.route('/')
+  // .get(function(req, res, next) {
+  //   res.json('Hello World');
+  // });
 }
