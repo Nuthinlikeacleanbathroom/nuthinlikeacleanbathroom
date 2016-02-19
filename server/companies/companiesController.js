38c00958966119dce89e46e3ac6f8bfeb049d4d4
John Soo <jsoo1@asu.edu>
2016-02-19 13:57:44 -0800

    (feat) Created endpoint to /companies to get city/state/name from crunchbase

---
+++ server/companies/companiesController.js
@@ -0,0 +1,17 @@
+var Companies = require('./companiesModel');
+
+var getAll = function(req, res) {
+  Companies.query('select name, state_code, city from cb_objects where state_code is NOT NULL && city is NOT NULL limit 100', 
+    function(err, results) {
+      if (err) {
+        return console.log('There was an error querying the database: ', err);
+      }
+      
+      console.log(results);
+      res.send(200, results);
+    });
+};
+
+module.exports = {
+  getAll: getAll
+};
