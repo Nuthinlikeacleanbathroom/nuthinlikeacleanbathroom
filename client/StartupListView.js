f9c01dc8b3a08b203db0a56405d184acdb85153f
John Soo <jsoo1@asu.edu>
2016-02-19 16:22:22 -0800

    Changed?

---
+++ client/StartupListView.js
@@ -0,0 +1,19 @@
+var StartupListView = Backbone.View.extend({
+	tagName: "table",
+	initialize: function(StartupCollection){
+		this.collection = StartupCollection;
+		this.render();
+	},
+
+	render: function(){
+		this.$el.children().detach();
+    		console.log(this.collection);
+		this.$el.html('<th></th>').append(
+			this.collection.map(function(startup){
+        			console.log('creatingstartupviews');
+				return new StartupView({model: startup}).render();
+			})
+		)
+	}
+
+})
