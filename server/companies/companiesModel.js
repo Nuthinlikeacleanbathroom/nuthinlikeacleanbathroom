27fd9f8af1014cc4c47ea147db4ac69455cca3c5
John Soo <jsoo1@asu.edu>
2016-02-22 19:21:24 -0800

    (feat)Added cleardb uri to companiesModel

--- server/companies/companiesModel.js
+++ server/companies/companiesModel.js
@@ -1,7 +1,9 @@
 var mysql = require('mysql');
 
+var mysqlUri = process.env.CLEARDB_DATABASE_URI || 'localhost';
+
 var connection = mysql.createConnection({
-  host: 'localhost',
+  host: mysqlUri,
   user: 'root',
   password: '',
   database: 'crunchbase'
