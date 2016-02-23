bcf12aae57815e977f9389c60b0038632a5bf5fa
John Soo <jsoo1@asu.edu>
2016-02-23 12:34:50 -0800

    (feat)removed favicon route

--- server/companies/companiesModel.js
+++ server/companies/companiesModel.js
@@ -2,7 +2,7 @@ var mysql = require('mysql');
 
 // Set connection per dev or deployed version.
 var mysqlPass, mysqlUser, mysqlDatabase, mysqlUri;
-if (process.env.PROCESS === 'production') {
+if (process.env.NODE_ENV === 'production') {
   mysqlPass = '7fbb12d0';
   mysqlUser = 'bd358f5252fa06';
   mysqlDatabase = 'heroku_2df98b5f307ebb3';
