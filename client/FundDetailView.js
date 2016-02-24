e36dd580df372d2052de1cfcada379e13f63fffb
Ian Akers <ian.asher.akers@gmail.com>
2016-02-24 18:05:17 -0500

    made date more readable

--- client/FundDetailView.js
+++ client/FundDetailView.js
@@ -1,11 +1,11 @@
 var FundDetailView = Backbone.View.extend({
-  
+
   template: _.template(
     '<div class="fundDetail">\
       <h2><%= name %></h2>\
       <h4 style="display:inline">Location:     </h4><%= city %>,<%= state_code %><br>\
       <h4 style="display:inline">Fund Homepage:     </h4><a href=<%= homepage_url %> ><%= homepage_url %></a><br>\
-      <h4 style="display:inline">Founded:     </h4><%= founded_at %><br>\
+      <h4 style="display:inline">Founded:     </h4><%= new Date(founded_at).toDateString() %><br>\
       <h4 style="display:inline">Companies Invested:     </h4><%= invested_companies %><br>\
       <h4 style="display:inline">Investment Rounds:     </h4><%= investment_rounds %><br>\
       <h4>Overview:</h4>\
