afa4c323985809cd833de72ea5dfb8890fc8862e
John Soo <jsoo1@asu.edu>
2016-02-24 15:50:29 -0800

    Added config for heroku

---
+++ server/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mysqlPass: process.env.CLEARDB_DATABASE_PASS || '7fbb12d0',
+  mysqlUser: process.env.CLEARDB_DATABASE_USER || 'bd358f5252fa06',
+  mysqlDatabase: process.env.CLEARDB_DATABASE || 'heroku_2df98b5f307ebb3'
+};
