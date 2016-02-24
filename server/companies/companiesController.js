ce9e5992b3a5bf910c396d3d88161a3cdd1b20fa
JT Yim <JT@JTYim.com>
2016-02-24 18:17:34 -0500

    merged conflicts

--- server/companies/companiesController.js
+++ server/companies/companiesController.js
@@ -3,15 +3,6 @@ var Companies = require('./companiesModel');
 // Queries DB for all companies for name, city and state columns. This serves the GET request to '/companies'
 var getAll = function(req, res) {
 
-<<<<<<< HEAD
-  Companies.query('select * from fund_list', 
-    function(err, results) {
-      if (err) {
-        return console.log('There was an error querying the database: ', err);
-      }
-      res.status(200).json(results);
-    });
-=======
   Companies.query('select * from fund_list', function(err, results) {
     if (err) {
       console.log('There was an error querying the database:', err);
@@ -19,7 +10,6 @@ var getAll = function(req, res) {
     }
     res.status(200).json(results);
   });
->>>>>>> 230925203d92e5b35363f3654b813a43cc6b8f22
 
 };
 
