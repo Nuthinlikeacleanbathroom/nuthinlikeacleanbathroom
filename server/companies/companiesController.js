4965daa7222ad1d201ab0259e2e45187205ed3ea
Alex Ko <alexko@utexas.edu>
2016-02-24 18:44:40 -0700

    testing re-requiring conenction object

--- server/companies/companiesController.js
+++ server/companies/companiesController.js
@@ -1,18 +1,18 @@
-var Companies = require('./companiesModel');
+//var Companies = require('./companiesModel');
 
 // Queries DB for all companies for name, city and state columns. This serves the GET request to '/companies'
 var getAll = function(req, res) {
   //Companies.connect();
-  Companies.query('select * from fund_list', function(err, results) {
+  require('./companiesModel').query('select * from fund_list', function(err, results) {
     if (err) {
       console.log('There was an error querying the database:', err);
-      console.log(Companies.testtest);
+      console.log(require('./companiesModel').testtest);
 
       return res.send(500);
     }
     //Companies.end();
-      console.log(Companies.testtest);
-    
+      console.log(require('./companiesModel').testtest);
+
     res.status(200).json(results);
   });
 };
