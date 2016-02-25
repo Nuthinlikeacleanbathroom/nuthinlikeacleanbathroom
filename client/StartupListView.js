3db4e40a1790d7676e27ac67cf3ddcbf645d9496
JT Yim <JT@JTYim.com>
2016-02-24 20:58:35 -0500

    merge

--- client/StartupListView.js
+++ client/StartupListView.js
@@ -1,15 +1,36 @@
 var StartupListView = Backbone.View.extend({
   tagName: 'div',
 
+  $x : $('<form action="" id="" method="">\
+        <input type="" name="" class="searchValue" value="Search"/>\
+        <button type="submit" class="findFunds">SEARCH</button>\
+    </form>'), 
+
   initialize: function(){
     this.render();
   },
 
+  events: {
+      'click button.findFunds': 'search',
+  },
+
   render: function(){
     this.$el.children().detach();
+    this.$el.append(this.$x);
     this.$el.append(this.collection.map(function(startup){
       return new StartupView({model: startup}).render();
     }));
     return this.$el;
-  }
+  },
+
+  search: function (){
+    var searchName = $('.searchValue').val()
+    // debugger;
+    console.log(searchName);
+    // var filtered = this.startups.where({ name: searchName});
+    // console.log(filtered);
+    // this.searchresultsView = new StartupListView({model: startup});
+  },
+
+
 })
