aaf3ca165bffe05f6d394612b9c07058a7eda826
John Soo <jsoo1@asu.edu>
2016-02-24 19:57:27 -0800

    fixed typos in companiesController

--- server/companies/companiesController.js
+++ server/companies/companiesController.js
@@ -2,7 +2,6 @@
 var getAll = function(req, res) {
   //ClearDB disconnects idle connections so the newest connection must be used on every query
   require('./companiesModel').query('select * from fund_list', function(err, results) {
-  Companies.query('select * from fund_list', function(err, results) {
     if (err) {
       console.log('There was an error querying the database:', err);
       return res.send(500);
@@ -10,8 +9,8 @@ var getAll = function(req, res) {
     
     res.status(200).json(results);
   });
-};
 
+};
 
 module.exports = {
   getAll: getAll
