d2507e3493c3db0e04113f2560d542471fd9f1db
JT Yim <JT@JTYim.com>
2016-02-24 22:25:08 -0500

    temp

--- server/companies/companiesModel.js
+++ server/companies/companiesModel.js
@@ -4,23 +4,24 @@ var config = require('../config.js');
 
 var connection = mysql.createConnection(config);
 
-var connection = mysql.createConnection({
-  host: mysqlUri,
-  user: mysqlUser,
-  password: mysqlPass,
-  database: mysqlDatabase
-});
+// var connection = mysql.createConnection({
+//   host: mysqlUri,
+//   user: mysqlUser,
+//   password: mysqlPass,
+//   database: mysqlDatabase
+// });
 
 //ClearDB will disconnect idle connections so a new connection object needs to be created and connected
 connection.on('error', function(err) {
   console.log('Database error', err);
   if (err.code === 'PROTOCOL_CONNECTION_LOST') {
-    var newConnection = mysql.createConnection({
-      host: mysqlUri,
-      user: mysqlUser,
-      password: mysqlPass,
-      database: mysqlDatabase
-    });
+    // var newConnection = mysql.createConnection({
+    //   host: mysqlUri,
+    //   user: mysqlUser,
+    //   password: mysqlPass,
+    //   database: mysqlDatabase
+    // });
+    var newConnection = mysql.createConnection(config);
     module.exports = newConnection;
   } else {
     throw err;
