04107f95cbfa4d44e1dd52485eba6795b10d2631
Alex Ko <alexko@utexas.edu>
2016-02-24 18:39:49 -0700

    testing if exports changes object references correctly

--- server/companies/companiesController.js
+++ server/companies/companiesController.js
@@ -6,9 +6,13 @@ var getAll = function(req, res) {
   Companies.query('select * from fund_list', function(err, results) {
     if (err) {
       console.log('There was an error querying the database:', err);
+      console.log(Companies.testtest);
+
       return res.send(500);
     }
     //Companies.end();
+      console.log(Companies.testtest);
+    
     res.status(200).json(results);
   });
 };
