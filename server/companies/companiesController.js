d89683e73b01427db6ab984feea889ecd342279a
Alex Ko <alexko@utexas.edu>
2016-02-24 18:19:21 -0700

    testing without connection and end

--- server/companies/companiesController.js
+++ server/companies/companiesController.js
@@ -2,11 +2,12 @@
 var getAll = function(req, res) {
   //ClearDB disconnects idle connections so the newest connection must be used on every query
   require('./companiesModel').query('select * from fund_list', function(err, results) {
+  Companies.query('select * from fund_list', function(err, results) {
     if (err) {
       console.log('There was an error querying the database:', err);
       return res.send(500);
     }
-    Companies.end();
+    //Companies.end();
     res.status(200).json(results);
   });
 
