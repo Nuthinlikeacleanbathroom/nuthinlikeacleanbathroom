d1da983557cddf346d2e0db06e108391210dc1bf
Alex Ko <alexko@utexas.edu>
2016-02-24 18:32:32 -0700

    testing using new connection object on error

--- server/companies/companiesModel.js
+++ server/companies/companiesModel.js
@@ -20,4 +20,19 @@ connection.on('error', function(err) {
 });
 //connection.connect();
 
+connection.on('error', function(err) {
+  console.log('Database error', err);
+  if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+    var connection = mysql.createConnection({
+      host: mysqlUri,
+      user: mysqlUser,
+      password: mysqlPass,
+      database: mysqlDatabase
+    });
+    module.exports = connection;
+  } else {
+    throw err;
+  }
+});
+
 module.exports = connection;
