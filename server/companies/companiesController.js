2b57fb34ca391255a3ca5fe48cba86cbcaee78d0
Alex Ko <alexko@utexas.edu>
2016-02-24 18:13:12 -0700

    testing connecting and reconnecting on api requests

--- server/companies/companiesController.js
+++ server/companies/companiesController.js
@@ -2,11 +2,13 @@ var Companies = require('./companiesModel');
 
 // Queries DB for all companies for name, city and state columns. This serves the GET request to '/companies'
 var getAll = function(req, res) {
+  Companies.connect();
   Companies.query('select * from fund_list', function(err, results) {
     if (err) {
       console.log('There was an error querying the database:', err);
       return res.send(500);
     }
+    Companies.end();
     res.status(200).json(results);
   });
 };
