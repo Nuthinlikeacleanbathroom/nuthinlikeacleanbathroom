7f2b90b345695f0f5b758e816ba5061ffc23239a
John Soo <jsoo1@asu.edu>
2016-02-26 15:12:57 -0800

    Renamed server funds dir

---
+++ server/Funds/fundsModel.js
@@ -0,0 +1,25 @@
+var mysql = require('mysql');
+// Sets connection per environment
+var config = require('../config.js');
+
+var connection = mysql.createConnection(config);
+
+console.time('uptime');
+
+//ClearDB will disconnect idle connections so a new connection object needs to be created and connected
+connection.on('error', function(err) {
+  console.log('Database error', err);
+  if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+    console.timeEnd('uptime');
+    console.time('uptime');
+    console.log('Reconnecting to server');
+    var newConnection = mysql.createConnection(config);
+
+    module.exports = newConnection;
+  } else {
+    throw err;
+  }
+});
+
+
+module.exports = connection;
