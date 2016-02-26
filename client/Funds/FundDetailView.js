1df4ff5f76a7bac0d26d74dc565e7b4ac3e0274a
John Soo <jsoo1@asu.edu>
2016-02-25 16:38:37 -0800

    (feat)Added viz to fundDetailViews. Renders circles based on fund investments and investment rounds

--- client/Funds/FundDetailView.js
+++ client/Funds/FundDetailView.js
@@ -1,14 +1,14 @@
 var FundDetailView = Backbone.View.extend({
 
   template: _.template(
-    '<div class="fundDetail container-fluid" data-detail="<%= id %>">' +
+    '<div class="fundDetail container-fluid">' +
       '<h2 class="text-center"><%= name %></h2>' +
       '<div><strong>Location:</strong> <%= city %>,<%= state_code %></div>' +
       '<div><strong>Fund Homepage:</strong> <a href=<%= homepage_url %> ><%= homepage_url %></a></div>' +
       '<div><strong>Founded:</strong> <%= new Date(founded_at).toDateString() %></div>' +
       '<div><strong>Companies Invested:</strong> <%= invested_companies %></div>' +
       '<div><strong>Investment Rounds:</strong> <%= investment_rounds %></div>' +
-      '<div class="plot">' +
+      '<div class="plot" data-detail="<%= id %>">' +
       '</div>' +
       '<br/><div><strong>Overview:</strong> <p><%= overview %></div>' +
     '</div>'),
@@ -19,27 +19,50 @@ var FundDetailView = Backbone.View.extend({
   },
 
   render: function(){
-    return this.$el.html(this.template(this.model.attributes));
-  },
-  
-  plotData: function(){    
-    var plot = d3.select('.fundDetail[data-detail="' + this.model.get('id') + '"].plot')
+    var $html = this.$el.html(this.template(this.model.attributes));
+    this.plot = d3.select($html[0]).select('.plot')
       .append('svg')
       .attr('background-color', '#000')
-      .attr('class', 'fundPlot')
-      .attr('heigth', 100)
-      .attr('width', 200);
-    var data = plot.selectAll('.datum')
-      .data([this.model.get('invested_companies'), this.model.get('funding_rounds')]);
+      .attr('heigth', '100%')
+      .attr('width', '100%');
+    return $html;
+  },
+  
+  plotData: function(){
+    this.plot.selectAll('circle').remove();
+    
+    var fundData = [
+      {
+        name: 'rounds',
+        val: this.model.get('investment_rounds')
+      },
+      {
+        name: 'companies',
+        val: this.model.get('invested_companies')
+      }
+    ];
+    
+    var data = this.plot.selectAll('.datum')
+      .data(fundData);
     
     data.enter().append('circle')
-      .attr('class', 'datum')
-      .attr('cy', 50)
-      .attr('cx', 100)
-      .attr('r', function(datum) {
-        return datum;
-      });
+      .attr('class', function(datum) {
+        return datum.name;
+      })
+      .attr('cy', '50%')
+      .attr('cx', '50%')
+      .attr('background-color', function(datum) {
+        return datum.name === 'rounds' ? '#537' : '#AAC';
+      })
+      .attr('opacity', .5)
+      .attr('r', 0);
       
-    data.exit().remove();
+    this.plot.selectAll('circle')
+      .transition()
+      .attr('r', function(datum) {
+        return datum.val / 50 * 8;
+      })
+      .delay(100)
+      .duration(250);      
   }
 });
